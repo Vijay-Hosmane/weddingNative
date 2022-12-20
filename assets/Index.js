9b354a859c1bf74bdba0d67181ed0bd1f6e735ab
Vijay-Hosmane <vijay.hosmane@cognitiveclouds.com>
2022-12-20 23:17:10 +0530

    removed git ignore

---
+++ assets/Index.js
@@ -0,0 +1,61 @@
+import weddingCard from '../assets/weddingCard.jpg';
+import receptionCard from '../assets/receptionCard.webp';
+import backgroundImage from '../assets/backgroundImage.webp';
+import back from '../assets/back.png';
+import Home from '../assets/Home.png';
+import play from '../assets/play.png';
+import reception1 from '../assets/reception1.jpg';
+import reception2 from '../assets/reception2.jpg';
+import reception3 from '../assets/reception3.jpg';
+import reception4 from '../assets/reception4.jpg';
+import reception5 from '../assets/reception5.jpg';
+import wedding1 from '../assets/wedding1.jpeg';
+import wedding2 from '../assets/wedding2.jpeg';
+import wedding3 from '../assets/wedding3.jpg';
+import wedding4 from '../assets/wedding4.jpeg';
+import wedding5 from '../assets/wedding5.jpg';
+
+import t1 from '../assets/t1.png';
+import t2 from '../assets/t2.png';
+import t3 from '../assets/t3.png';
+import t4 from '../assets/t4.png';
+import t5 from '../assets/t5.png';
+import t6 from '../assets/t6.png';
+import vid1 from '../assets/vid1.mp4';
+import vid2 from '../assets/vid2.mp4';
+import vid3 from '../assets/vid3.mp4';
+import vid4 from '../assets/vid4.mp4';
+import vid5 from '../assets/vid5.mp4';
+import vid6 from '../assets/v6.mp4';
+import videosDisplayBackground from '../assets/videosDisplayBackground.jpeg';
+export {
+  vid1,
+  vid2,
+  vid3,
+  vid4,
+  vid5,
+  vid6,
+  t1,
+  t2,
+  t3,
+  t4,
+  t5,
+  t6,
+  videosDisplayBackground,
+  backgroundImage,
+  back,
+  Home,
+  play,
+  weddingCard,
+  receptionCard,
+  reception1,
+  reception2,
+  reception3,
+  reception4,
+  reception5,
+  wedding1,
+  wedding2,
+  wedding3,
+  wedding4,
+  wedding5,
+};
